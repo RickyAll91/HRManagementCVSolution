38dbed6cb52f32477b49af8c81042d0096525e66
Riccardo Allegretti <allric91@outlook.it>
2023-08-16 16:43:12 +0200

    FIxed missing dependency

--- HRManagement/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ HRManagement/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,16 +2,23 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using HRManagement.Shared.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-using System.Text.Encodings.Web;
-using HRManagement.Shared.Models;
+using Microsoft.Extensions.Logging;
 
 namespace HRManagement.Server.Areas.Identity.Pages.Account
 {
